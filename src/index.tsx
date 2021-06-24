86308c17f166d2dcd33a1b3b0dad75610d10da1c
Brian Li <brnli7@gmail.com>
2021-06-24 14:35:23 -0700

    Remove deps

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import theme from "theme";
 import App from "App";
-import { Web3Provider } from "@ethersproject/providers";
-import { Web3ReactProvider, createWeb3ReactRoot } from "@web3-react/core";
 import { ThemeProvider } from "theme-ui";
 import { BrowserRouter } from "react-router-dom";
 import { CHAIN_ID } from "config";
@@ -24,37 +22,23 @@ declare global {
   }
 }
 
-export const NetworkContextName = "NETWORK";
-
-function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider);
-  library.pollingInterval = 15000;
-  return library;
-}
-
-const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
-
 ReactDOM.render(
   <React.StrictMode>
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Web3ProviderNetwork getLibrary={getLibrary}>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <ContractKitProvider
-              dappName="Poof.cash deployer"
-              dappDescription="dApp for deploying the Poof smart contracts"
-              dappUrl={window.location.href.slice(
-                0,
-                window.location.href.length - 1
-              )}
-              networks={[CHAIN_ID === ChainId.MAINNET ? Mainnet : Alfajores]}
-            >
-              <App />
-            </ContractKitProvider>
-          </BrowserRouter>
-        </ThemeProvider>
-      </Web3ProviderNetwork>
-    </Web3ReactProvider>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <ContractKitProvider
+          dappName="Poof.cash deployer"
+          dappDescription="dApp for deploying the Poof smart contracts"
+          dappUrl={window.location.href.slice(
+            0,
+            window.location.href.length - 1
+          )}
+          networks={[CHAIN_ID === ChainId.MAINNET ? Mainnet : Alfajores]}
+        >
+          <App />
+        </ContractKitProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
